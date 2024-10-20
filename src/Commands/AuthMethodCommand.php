c01530b741c241e7cbfe3d0c98b097833a3d6bd7
Diego Werlich <dwerlich21@gmail.com>
2024-10-20 10:13:48 -0300

    inicio do sistema de autenticacao

--- src/Commands/AuthMethodCommand.php
+++ src/Commands/AuthMethodCommand.php
@@ -36,16 +36,103 @@ class AuthMethodCommand extends Command
     public function handle(): void
     {
         // Prepare structure
-        $model = ucfirst($this->argument('model'));
-        $this->makeMigration($model);
-        $this->makeModel($model);
-        $this->makeRepository($model);
-        $this->makeService($model);
-        $this->makeRequest($model);
-        $this->makeController($model);
-        $this->appendRoute($model);
+        $this->makeController();
+
 
         // Success Message
-        $this->info('Vê se segue os padrões heein!');
+        $this->info('Sistema de autenticação criado!');
+    }
+
+    public function makeController()
+    {
+        $controller = <<<EOF
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use App\Services\UserService;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController
+{
+    /**
+     * @var UserService
+     */
+    protected \$service;
+
+    /**
+     * AuthController constructor.
+     *
+     * @param UserService \$service
+     */
+    public function __construct(UserService \$service)
+    {
+        \$this->service = \$service;
+    }
+
+    /**
+     * Authenticates a user and issues a token.
+     *
+     * @param Request \$request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function auth(Request \$request): JsonResponse
+    {
+        \$loginUserData = \$request->validate([
+            'email'=>'required|string|email',
+            'password'=>'required|min:8'
+        ]);
+
+        \$user = User::where('email',\$loginUserData['email'])
+            ->where('status', 1)
+            ->first();
+
+        if(!\$user || !Hash::check(\$loginUserData['password'],\$user->password)){
+            return response()->json([
+                'message' => 'E-mail ou senha inválidos!'
+            ],401);
+        }
+
+        \$expirationMinutes = config('sanctum.expiration', 180);
+
+        \$token = \$user->createToken('AdminToken', [], Carbon::now()->addMinutes(\$expirationMinutes))->plainTextToken;
+        return response()->json([
+            'token' => \$token,
+        ]);
+    }
+
+    /**
+     * Get the authenticated user.
+     * @return JsonResponse
+     */
+    public function me(): JsonResponse
+    {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Method to invalidate the token.
+     *
+     * @param Request \$request
+     * @return JsonResponse
+     */
+    public function logout(Request \$request): JsonResponse
+    {
+        auth()->guard('admin')->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logout efetuado.'], 200);
+    }
+}
+
+EOF;
+        file_put_contents(app_path("Http/Controllers/Api/AuthController.php"), $controller);
+
     }
 }
